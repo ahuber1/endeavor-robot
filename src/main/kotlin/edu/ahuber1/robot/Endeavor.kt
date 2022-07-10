1bc93284a6cafde9758c1aa75e67b231eed177bd
Andrew Huber <ahuber1@umbc.edu>
2022-07-10 19:45:23 -0400

    Commented out Endeavor.kt and RobotHelpers.kt

--- src/main/kotlin/edu/ahuber1/robot/Endeavor.kt
+++ src/main/kotlin/edu/ahuber1/robot/Endeavor.kt
@@ -1,182 +1,182 @@
-package edu.ahuber1.robot
-
-import edu.ahuber1.math.Point
-import edu.ahuber1.math.projectPoint
-import edu.ahuber1.math.toDegrees
-import edu.ahuber1.math.toRadians
-import robocode.*
-import java.util.concurrent.locks.ReentrantLock
-import kotlin.concurrent.withLock
-
-public class Endeavor : TeamRobot() {
-
-    private enum class RotationDirection { CLOCKWISE, COUNTERCLOCKWISE }
-    private data class Enemy(val name: String, val location: Point, val engagementOrder: EngagementOrder? = null)
-    private data class EngagementOrder(val enemy: Enemy, val bulletPower: Double)
-
-    private val lock = ReentrantLock()
-    private val enemies = HashMap<String, Enemy>()
-    private var radarRotationDirection: RotationDirection? = null
-
-    private inline val currentLocation: Point
-        get() = Point(x, y)
-
-    override fun onHitRobot(event: HitRobotEvent?) {
-        super.onHitRobot(event)
-
-        if (event == null) {
-            return
-        }
-
-        lock.withLock {
-            val hitEdges = getHitEdges(headingRadians, event.bearingRadians)
-            println("Hit edges: $hitEdges")
-
-            val farthest = getFarthestWall(currentLocation, battlefieldSize, hitEdges.opposite)
-            println("Farthest: $farthest")
-
-            val turnAmountRadians = farthest.radians - headingRadians
-            println("Turn amount: ${toDegrees(turnAmountRadians)}")
-
-            turnRightRadians(turnAmountRadians)
-            ahead(advanceDistance)
-        }
-    }
-
-    override fun onHitWall(event: HitWallEvent?) {
-        super.onHitWall(event)
-
-        if (event == null) {
-            return
-        }
-
-        lock.withLock {
-            val closestWall = getClosestWall(currentLocation, battlefieldSize)
-            val turnAmountRadians = closestWall.opposite.radians - headingRadians
-
-            turnRightRadians(turnAmountRadians)
-            ahead(advanceDistance)
-        }
-    }
-
-    override fun onScannedRobot(event: ScannedRobotEvent?) {
-        super.onScannedRobot(event)
-
-        if (event == null || shouldIgnoreRobot(event)) {
-            return
-        }
-
-        lock.withLock {
-            val enemyLocation = projectPoint(currentLocation, event.distance, event.bearingRadians)
-            enemies[event.name] =
-                enemies[event.name]?.copy(location = enemyLocation) ?: Enemy(event.name, enemyLocation)
-        }
-    }
-
-    private fun shouldIgnoreRobot(event: ScannedRobotEvent): Boolean {
-        // TODO: Ignore sentry robots?
-        return isTeammate(event.name)
-    }
-
-    override fun onRobotDeath(event: RobotDeathEvent?) {
-        super.onRobotDeath(event)
-
-        if (event == null) {
-            return
-        }
-
-        lock.withLock {
-            enemies.remove(event.name)
-        }
-    }
-
-    override fun run() {
-        super.run()
-
-        isAdjustGunForRobotTurn = true
-        isAdjustRadarForRobotTurn = true
-
-        while (true) {
-            lock.withLock(::updateRobot)
-        }
-    }
-
-    private fun updateRobot() {
-        updateRadar()
-
-        val enemyName = selectEnemyToEngage() ?: return
-        attack(enemyName)
-    }
-
-    private fun updateRadar() {
-        var radarRotationDirection = this.radarRotationDirection
-
-        // If the radar isn't spinning, determine which direction to spin the radar so we reach the battlefield center
-        // as quickly as possible.
-        if (radarRotationDirection == null) {
-            val shortestAngleRadians = Point.angleRadians(currentLocation, battlefieldCenter)
-
-            radarRotationDirection = when {
-                shortestAngleRadians < 0 -> RotationDirection.COUNTERCLOCKWISE
-                else -> RotationDirection.CLOCKWISE
-            }
-
-            this.radarRotationDirection = radarRotationDirection
-        }
-
-        // Start rotating the radar, 45 degrees per turn
-        val rotationAmount = 360.0
-        val rotationAngleRadians = when (radarRotationDirection) {
-            RotationDirection.CLOCKWISE -> toRadians(rotationAmount)
-            RotationDirection.COUNTERCLOCKWISE -> -toRadians(rotationAmount)
-        }
-
-        turnRadarRightRadians(rotationAngleRadians)
-    }
-
-    /**
-     * Analyzes [enemies] and returns the name of the enemy to engage or `null` if no enemy should be engaged.
-     */
-    private fun selectEnemyToEngage(): String? {
-        val engagementOrder = findShortestEnemyToEngage() ?: return null
-
-        enemies[engagementOrder.enemy.name] =
-            enemies[engagementOrder.enemy.name]!!.copy(engagementOrder = engagementOrder)
-
-        return engagementOrder.enemy.name
-    }
-
-    /**
-     * Finds the enemy in [enemies] that will take the shortest amount of time to shoot.
-     */
-    private fun findShortestEnemyToEngage(): EngagementOrder? {
-        // TODO: Revise algorithm to predict where the enemy will be in the future.
-        return enemies.values.map { EngagementOrder(it, 3.0) }
-            .minByOrNull { Point.distance(it.enemy.location, currentLocation) }
-    }
-
-    private fun attack(enemyName: String) {
-        val engagementOrder = enemies[enemyName]?.engagementOrder ?: return
-
-        // Rotate gun
-        val angleRadians = Point.angleRadians(currentLocation, engagementOrder.enemy.location)
-        turnGunRightRadians(angleRadians)
-
-        // Fire gun
-        fire(engagementOrder.bulletPower)
-
-        // Advance on enemy
-        val rotationAngleRadians = Point.angleRadians(currentLocation, engagementOrder.enemy.location)
-        turnRightRadians(rotationAngleRadians)
-
-        val destination = projectPoint(currentLocation, advanceDistance, headingRadians)
-        val canAdvance = enemies.values.none { Point.distance(destination, it.location) <= 120 }
-        if (canAdvance) {
-            ahead(advanceDistance)
-        }
-    }
-
-    public companion object {
-        private const val advanceDistance: Double = 20.0
-    }
-}
+//package edu.ahuber1.robot
+//
+//import edu.ahuber1.math.Point
+//import edu.ahuber1.math.projectPoint
+//import edu.ahuber1.math.toDegrees
+//import edu.ahuber1.math.toRadians
+//import robocode.*
+//import java.util.concurrent.locks.ReentrantLock
+//import kotlin.concurrent.withLock
+//
+//public class Endeavor : TeamRobot() {
+//
+//    private enum class RotationDirection { CLOCKWISE, COUNTERCLOCKWISE }
+//    private data class Enemy(val name: String, val location: Point, val engagementOrder: EngagementOrder? = null)
+//    private data class EngagementOrder(val enemy: Enemy, val bulletPower: Double)
+//
+//    private val lock = ReentrantLock()
+//    private val enemies = HashMap<String, Enemy>()
+//    private var radarRotationDirection: RotationDirection? = null
+//
+//    private inline val currentLocation: Point
+//        get() = Point(x, y)
+//
+//    override fun onHitRobot(event: HitRobotEvent?) {
+//        super.onHitRobot(event)
+//
+//        if (event == null) {
+//            return
+//        }
+//
+//        lock.withLock {
+//            val hitEdges = getHitEdges(headingRadians, event.bearingRadians)
+//            println("Hit edges: $hitEdges")
+//
+//            val farthest = getFarthestWall(currentLocation, battlefieldSize, hitEdges.opposite)
+//            println("Farthest: $farthest")
+//
+//            val turnAmountRadians = farthest.radians - headingRadians
+//            println("Turn amount: ${toDegrees(turnAmountRadians)}")
+//
+//            turnRightRadians(turnAmountRadians)
+//            ahead(advanceDistance)
+//        }
+//    }
+//
+//    override fun onHitWall(event: HitWallEvent?) {
+//        super.onHitWall(event)
+//
+//        if (event == null) {
+//            return
+//        }
+//
+//        lock.withLock {
+//            val closestWall = getClosestWall(currentLocation, battlefieldSize)
+//            val turnAmountRadians = closestWall.opposite.radians - headingRadians
+//
+//            turnRightRadians(turnAmountRadians)
+//            ahead(advanceDistance)
+//        }
+//    }
+//
+//    override fun onScannedRobot(event: ScannedRobotEvent?) {
+//        super.onScannedRobot(event)
+//
+//        if (event == null || shouldIgnoreRobot(event)) {
+//            return
+//        }
+//
+//        lock.withLock {
+//            val enemyLocation = projectPoint(currentLocation, event.distance, event.bearingRadians)
+//            enemies[event.name] =
+//                enemies[event.name]?.copy(location = enemyLocation) ?: Enemy(event.name, enemyLocation)
+//        }
+//    }
+//
+//    private fun shouldIgnoreRobot(event: ScannedRobotEvent): Boolean {
+//        // TODO: Ignore sentry robots?
+//        return isTeammate(event.name)
+//    }
+//
+//    override fun onRobotDeath(event: RobotDeathEvent?) {
+//        super.onRobotDeath(event)
+//
+//        if (event == null) {
+//            return
+//        }
+//
+//        lock.withLock {
+//            enemies.remove(event.name)
+//        }
+//    }
+//
+//    override fun run() {
+//        super.run()
+//
+//        isAdjustGunForRobotTurn = true
+//        isAdjustRadarForRobotTurn = true
+//
+//        while (true) {
+//            lock.withLock(::updateRobot)
+//        }
+//    }
+//
+//    private fun updateRobot() {
+//        updateRadar()
+//
+//        val enemyName = selectEnemyToEngage() ?: return
+//        attack(enemyName)
+//    }
+//
+//    private fun updateRadar() {
+//        var radarRotationDirection = this.radarRotationDirection
+//
+//        // If the radar isn't spinning, determine which direction to spin the radar so we reach the battlefield center
+//        // as quickly as possible.
+//        if (radarRotationDirection == null) {
+//            val shortestAngleRadians = Point.angleRadians(currentLocation, battlefieldCenter)
+//
+//            radarRotationDirection = when {
+//                shortestAngleRadians < 0 -> RotationDirection.COUNTERCLOCKWISE
+//                else -> RotationDirection.CLOCKWISE
+//            }
+//
+//            this.radarRotationDirection = radarRotationDirection
+//        }
+//
+//        // Start rotating the radar, 45 degrees per turn
+//        val rotationAmount = 360.0
+//        val rotationAngleRadians = when (radarRotationDirection) {
+//            RotationDirection.CLOCKWISE -> toRadians(rotationAmount)
+//            RotationDirection.COUNTERCLOCKWISE -> -toRadians(rotationAmount)
+//        }
+//
+//        turnRadarRightRadians(rotationAngleRadians)
+//    }
+//
+//    /**
+//     * Analyzes [enemies] and returns the name of the enemy to engage or `null` if no enemy should be engaged.
+//     */
+//    private fun selectEnemyToEngage(): String? {
+//        val engagementOrder = findShortestEnemyToEngage() ?: return null
+//
+//        enemies[engagementOrder.enemy.name] =
+//            enemies[engagementOrder.enemy.name]!!.copy(engagementOrder = engagementOrder)
+//
+//        return engagementOrder.enemy.name
+//    }
+//
+//    /**
+//     * Finds the enemy in [enemies] that will take the shortest amount of time to shoot.
+//     */
+//    private fun findShortestEnemyToEngage(): EngagementOrder? {
+//        // TODO: Revise algorithm to predict where the enemy will be in the future.
+//        return enemies.values.map { EngagementOrder(it, 3.0) }
+//            .minByOrNull { Point.distance(it.enemy.location, currentLocation) }
+//    }
+//
+//    private fun attack(enemyName: String) {
+//        val engagementOrder = enemies[enemyName]?.engagementOrder ?: return
+//
+//        // Rotate gun
+//        val angleRadians = Point.angleRadians(currentLocation, engagementOrder.enemy.location)
+//        turnGunRightRadians(angleRadians)
+//
+//        // Fire gun
+//        fire(engagementOrder.bulletPower)
+//
+//        // Advance on enemy
+//        val rotationAngleRadians = Point.angleRadians(currentLocation, engagementOrder.enemy.location)
+//        turnRightRadians(rotationAngleRadians)
+//
+//        val destination = projectPoint(currentLocation, advanceDistance, headingRadians)
+//        val canAdvance = enemies.values.none { Point.distance(destination, it.location) <= 120 }
+//        if (canAdvance) {
+//            ahead(advanceDistance)
+//        }
+//    }
+//
+//    public companion object {
+//        private const val advanceDistance: Double = 20.0
+//    }
+//}
--- src/main/kotlin/edu/ahuber1/robot/RobotHelpers.kt
+++ src/main/kotlin/edu/ahuber1/robot/RobotHelpers.kt
@@ -1,100 +1,100 @@
-package edu.ahuber1.robot
-
-import edu.ahuber1.math.*
-import robocode.Robot
-import robocode.StatusEvent
-import kotlin.math.abs
-
-/**
- * The center point of the battlefield.
- */
-public inline val Robot.battlefieldCenter: Point
-    get() = Point(battleFieldWidth / 2, battleFieldHeight / 2)
-
-/**
- * The size of the battlefield.
- */
-public inline val Robot.battlefieldSize: Size
-    get() = Size(battleFieldWidth, battleFieldHeight)
-
-/**
- * The location of the robot.
- */
-public inline val StatusEvent.robotLocation: Point
-    get() = Point(status.x, status.y)
-
-/**
- * Determines the edges where another robot hit your robot.
- * @param headingRadians Your heading in radians.
- * @param otherRobotBearingRadians The bearing of the other robot in radians. This is relative to your gun heading.
- */
-public fun getHitEdges(headingRadians: Double, otherRobotBearingRadians: Double): Set<Heading> {
-    // Convert the bearing of the other robot, which is relative to my heading, to an angle relative to the
-    // battlefield (0 radians is north).
-    val otherRobotHeading = normalizeRadians(otherRobotBearingRadians + headingRadians)
-
-    val otherIsAbove = otherRobotHeading !in toRadians(90.0)..toRadians(270.0)
-    val otherIsRight = otherRobotHeading in 0.0..toRadians(180.0)
-
-    val otherRobotVerticalEdge = if (otherIsAbove) Heading.NORTH else Heading.SOUTH
-    val otherRobotHorizontalEdge = if (otherIsRight) Heading.EAST else Heading.WEST
-
-    return setOf(otherRobotVerticalEdge, otherRobotHorizontalEdge)
-}
-
-/**
- * Returns a [Map] containing the straight-line distance to each of the provided walls from a point.
- * @param location The point from which to calculate the straight-line distance.
- * @param walls The walls from which to calculate the straight-line distance to [location].
- * @param battlefieldSize The size of the battlefield.
- */
-public fun getStraightLineDistanceToWalls(
-    location: Point,
-    walls: Iterable<Heading>,
-    battlefieldSize: Size
-): Map<Heading, Double> {
-    return walls.distinct().associateWith {
-        when (it) {
-            Heading.NORTH -> abs(battlefieldSize.height - location.y)
-            Heading.SOUTH -> abs(location.y)
-            Heading.WEST -> abs(location.x)
-            Heading.EAST -> abs(battlefieldSize.width - location.x)
-        }
-    }
-}
-
-/**
- * Returns the wall with the shortest straight-line distance.
- * @param location The point from which to calculate the straight-line distances.
- * @param battlefieldSize The size of the battlefield.
- * @param walls The walls from which to calculate the straight-line distance to [location].
- *
- * @see getStraightLineDistanceToWalls
- * @see getFarthestWall
- */
-public fun getClosestWall(
-    location: Point,
-    battlefieldSize: Size,
-    walls: Iterable<Heading> = Heading.values().asIterable()
-): Heading {
-    return getStraightLineDistanceToWalls(location, walls, battlefieldSize).minByOrNull { it.value }?.key
-        ?: error("There are no walls to choose from.")
-}
-
-/**
- * Returns the wall with the longest straight-line distance.
- * @param location The point from which to calculate the straight-line distances.
- * @param battlefieldSize The size of the battlefield.
- * @param walls The walls from which to calculate the straight-line distance to [location].
- *
- * @see getStraightLineDistanceToWalls
- * @see getClosestWall
- */
-public fun getFarthestWall(
-    location: Point,
-    battlefieldSize: Size,
-    walls: Iterable<Heading> = Heading.values().asIterable()
-): Heading {
-    return getStraightLineDistanceToWalls(location, walls, battlefieldSize).maxByOrNull { it.value }?.key
-        ?: error("There are no walls to choose from.")
-}
+//package edu.ahuber1.robot
+//
+//import edu.ahuber1.math.*
+//import robocode.Robot
+//import robocode.StatusEvent
+//import kotlin.math.abs
+//
+///**
+// * The center point of the battlefield.
+// */
+//public inline val Robot.battlefieldCenter: Point
+//    get() = Point(battleFieldWidth / 2, battleFieldHeight / 2)
+//
+///**
+// * The size of the battlefield.
+// */
+//public inline val Robot.battlefieldSize: Size
+//    get() = Size(battleFieldWidth, battleFieldHeight)
+//
+///**
+// * The location of the robot.
+// */
+//public inline val StatusEvent.robotLocation: Point
+//    get() = Point(status.x, status.y)
+//
+///**
+// * Determines the edges where another robot hit your robot.
+// * @param headingRadians Your heading in radians.
+// * @param otherRobotBearingRadians The bearing of the other robot in radians. This is relative to your gun heading.
+// */
+//public fun getHitEdges(headingRadians: Double, otherRobotBearingRadians: Double): Set<Heading> {
+//    // Convert the bearing of the other robot, which is relative to my heading, to an angle relative to the
+//    // battlefield (0 radians is north).
+//    val otherRobotHeading = normalizeRadians(otherRobotBearingRadians + headingRadians)
+//
+//    val otherIsAbove = otherRobotHeading !in toRadians(90.0)..toRadians(270.0)
+//    val otherIsRight = otherRobotHeading in 0.0..toRadians(180.0)
+//
+//    val otherRobotVerticalEdge = if (otherIsAbove) Heading.NORTH else Heading.SOUTH
+//    val otherRobotHorizontalEdge = if (otherIsRight) Heading.EAST else Heading.WEST
+//
+//    return setOf(otherRobotVerticalEdge, otherRobotHorizontalEdge)
+//}
+//
+///**
+// * Returns a [Map] containing the straight-line distance to each of the provided walls from a point.
+// * @param location The point from which to calculate the straight-line distance.
+// * @param walls The walls from which to calculate the straight-line distance to [location].
+// * @param battlefieldSize The size of the battlefield.
+// */
+//public fun getStraightLineDistanceToWalls(
+//    location: Point,
+//    walls: Iterable<Heading>,
+//    battlefieldSize: Size
+//): Map<Heading, Double> {
+//    return walls.distinct().associateWith {
+//        when (it) {
+//            Heading.NORTH -> abs(battlefieldSize.height - location.y)
+//            Heading.SOUTH -> abs(location.y)
+//            Heading.WEST -> abs(location.x)
+//            Heading.EAST -> abs(battlefieldSize.width - location.x)
+//        }
+//    }
+//}
+//
+///**
+// * Returns the wall with the shortest straight-line distance.
+// * @param location The point from which to calculate the straight-line distances.
+// * @param battlefieldSize The size of the battlefield.
+// * @param walls The walls from which to calculate the straight-line distance to [location].
+// *
+// * @see getStraightLineDistanceToWalls
+// * @see getFarthestWall
+// */
+//public fun getClosestWall(
+//    location: Point,
+//    battlefieldSize: Size,
+//    walls: Iterable<Heading> = Heading.values().asIterable()
+//): Heading {
+//    return getStraightLineDistanceToWalls(location, walls, battlefieldSize).minByOrNull { it.value }?.key
+//        ?: error("There are no walls to choose from.")
+//}
+//
+///**
+// * Returns the wall with the longest straight-line distance.
+// * @param location The point from which to calculate the straight-line distances.
+// * @param battlefieldSize The size of the battlefield.
+// * @param walls The walls from which to calculate the straight-line distance to [location].
+// *
+// * @see getStraightLineDistanceToWalls
+// * @see getClosestWall
+// */
+//public fun getFarthestWall(
+//    location: Point,
+//    battlefieldSize: Size,
+//    walls: Iterable<Heading> = Heading.values().asIterable()
+//): Heading {
+//    return getStraightLineDistanceToWalls(location, walls, battlefieldSize).maxByOrNull { it.value }?.key
+//        ?: error("There are no walls to choose from.")
+//}
